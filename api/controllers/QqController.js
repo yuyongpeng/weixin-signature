ef12d91c9324a7c5885eaf3b2d92e969691bbc52
yuyongpeng <yuyongpeng@modernmedia.com.cn>
2017-09-04 10:59:14 +0800

    update

--- api/controllers/QqController.js
+++ api/controllers/QqController.js
@@ -1,24 +1,3 @@
-
-function weixin(req, res) {
-  var url = req.query.url;
-  var appId = req.query.appId;
-
-  if (!url || !appId) {
-    return res.badRequest();
-  }
-  TokenService.getSign(url, appId).then(function(sign){
-    res.jsonp({
-      code: 200,
-      sign: sign
-    });
-  }).catch(function(err){
-    res.json({
-      code: 500,
-      err: err
-    })
-  });
-}
-
 function loginAction(req, res){
   var app_id = sails.config.thirdlogin.qq['nowness'].app_id;
   var redirect_url = sails.config.thirdlogin.qq['nowness'].redirect_url;
@@ -29,21 +8,10 @@ function loginAction(req, res){
 }
 
 function callbackAction(req, res){
-  return res.view('qqcallback',{});
-
-  var code = req.query.code;
-  var state = req.query.state;
-  sails.log('code=' + code);
-  sails.log('state=' + state);
-  WeixinService.getSign('nowness',code).then(function(data){
-    return res.view('weixincallback',{
-      userinfo: JSON.stringify(data)
-    });
-  });
+  return res.view('qqcallback',{userinfo: "test"});
 }
 
 module.exports = {
-    weixin: weixin,
     login: loginAction,
     callback: callbackAction
 }

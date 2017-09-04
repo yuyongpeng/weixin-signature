0297fec5a45b5c7717dac2445ffb72eeefbf8515
yuyongpeng <yuyongpeng@modernmedia.com.cn>
2017-09-04 11:34:28 +0800

    update

--- api/controllers/QqController.js
+++ api/controllers/QqController.js
@@ -11,7 +11,8 @@ function login2Action(req, res){
   var app_id = sails.config.thirdlogin.qq['nowness'].app_id;
   var redirect_url = sails.config.thirdlogin.qq['nowness'].redirect_url;
   url = 'https://graph.qq.com/oauth2.0/authorize?response_type=code&client_id=' + app_id + '&redirect_uri=' + encodeURIComponent(redirect_url) + '&state=124'
-  res.location(url)
+  //res.location(url);
+  res.redirect(url)
 }
 
 function callbackAction(req, res){

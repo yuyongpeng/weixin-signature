848d18c98a162104fe377e9de8dc6acfe9f90668
yuyongpeng <yuyongpeng@modernmedia.com.cn>
2017-09-20 13:19:42 +0800

    有了基本的样式

--- api/controllers/QiniuController.js
+++ api/controllers/QiniuController.js
@@ -8,28 +8,28 @@ function uploadFile(req, res) {
   })
 }
 
-function test(req, res){
+function test(req, res) {
   //sails.models.category.find().exec(function(err, all){
-//  Category.find().exec(function(err, all){
-//   if (err)
-//      sails.log(err);
-//    sails.log(all);
-//  });
+  //  Category.find().exec(function(err, all){
+  //   if (err)
+  //      sails.log(err);
+  //    sails.log(all);
+  //  });
   var category1 = {
     name: 'test1',
     lang: 'cn'
   };
-  Category.create(category1).exec(function(err, finn){
+  Category.create(category1).exec(function (err, finn) {
     if (err)
       sails.log(err);
     sails.log(finn);
   });
-  Category.find({id:33}).exec(function(err, all){
+  Category.find({ id: 33 }).exec(function (err, all) {
     sails.log(all);
     all[0].name = 'test222222';
     sails.log(all[0]);
-    Category.update({id: all[0].id}, all[0]).exec(function(err,finn){
-      if(err)
+    Category.update({ id: all[0].id }, all[0]).exec(function (err, finn) {
+      if (err)
         sails.log(err);
       sails.log(finn);
     });
@@ -42,14 +42,17 @@ function test(req, res){
  * @param {*} req 
  * @param {*} res 
  */
-function nownessvideo(req, res){
+function nownessvideo(req, res) {
   var domain = '7xpvul.com2.z0.glb.qiniucdn.com';
   var nownessid = req.body.nownessid || 0;
   var nownessname = req.body.nownessname;
   var title = req.body.title;
   var qiniufile = req.body.qiniufile;
+  var url = '';
+  if (qiniufile != undefined && qiniufile != "") {
+    url = 'http://' + domain + '/' + qiniufile;
+  }
   var file = qiniufile.split('-');
-
   var subtitle = req.body.subtitle;
   var category = req.body.category;
   var keyword = req.body.keyword;
@@ -69,20 +72,26 @@ function nownessvideo(req, res){
     email: email,
     debut: debut,
     originalfilename: file[1],
-    url: 'http://' + domain + '/' + qiniufile,
+    url: url,
     extvideourl: extvideourl,
     content: content
   };
-  Videofiles.create(videofile).exec(function(err, ret){
-    if (err)
+  Videofiles.create(videofile).exec(function (err, ret) {
+    if (err) {
+      res.negotiate(err);
       sails.log(err);
+    }
     sails.log(ret);
+    res.status(200);
+    // return res.location('http://localhost:1337/video/qiniuupload');
+    return res.status(200).send('成功<a href="/video/qiniuupload">返回</a>');
+//    return res.status(200).redirect('http://localhost:1337/video/qiniuupload');
   });
 }
 
 
 module.exports = {
-    uploadFile: uploadFile,
-    nownessvideo: nownessvideo,
-    test: test
+  uploadFile: uploadFile,
+  nownessvideo: nownessvideo,
+  test: test
 }
